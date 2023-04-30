a5e9045a2d21f4ca3d474aa20617fb87e8d919f6
wrathchild14 <jovanprodanov123@gmail.com>
2023-04-30 16:36:18 +0200

    Imporve bash script for calculating results

--- exercise8/run.sh
+++ exercise8/run.sh
@@ -1,6 +1,17 @@
 #!/bin/sh
 module load OpenMPI/4.1.0-GCC-10.2.0 
 mpicc pi_mpi.c -o pi_mpi -lm
-# -n# - processes -N# - nodes
-srun --reservation=fri --mpi=pmix -n8 -N2 ./pi_mpi
+
+processes=(1 2 4 8 16 32)
+nodes=(1 2)
+
+for n in "${nodes[@]}"
+do
+  for p in "${processes[@]}"
+  do
+    srun --reservation=fri --mpi=pmix -n $p -N $n ./pi_mpi
+    echo "Nodes: $n, Processes: $p"
+  done
+done
+
 rm pi_mpi

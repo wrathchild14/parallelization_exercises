e84cf3fe06c34fb35f2d867f02d3d8608d936a50
wrathchild14 <jovanprodanov123@gmail.com>
2023-04-30 16:36:27 +0200

    Add results

--- exercise8/pi_mpi.c
+++ exercise8/pi_mpi.c
@@ -5,7 +5,44 @@
 #include <string.h>
 
 #define SEED 42
-#define SAMPLES 50000000
+#define SAMPLES 5000000
+
+/* Readme:
+Samples = 5000000
+
+Nodes: 1, Processes: 1:
+PI = 4.000000 | computed in 0.00 s
+
+Nodes: 1, Processes: 2:
+PI = 3.571493 | computed in 0.25 s
+
+Nodes: 1, Processes: 4:
+PI = 3.357225 | computed in 0.14 s
+
+Nodes: 1, Processes: 8:
+PI = 3.250678 | computed in 0.13 s
+
+Nodes: 1, Processes: 16:
+PI = 3.196818 | computed in 0.04 s
+
+Nodes: 1, Processes: 32:
+PI = 3.169265 | computed in 0.02 s
+
+Nodes: 2, Processes: 2:
+PI = 3.571493 | computed in 0.27 s
+
+Nodes: 2, Processes: 4:
+PI = 3.357225 | computed in 0.20 s
+
+Nodes: 2, Processes: 8:
+PI = 3.250678 | computed in 0.08 s
+
+Nodes: 2, Processes: 16:
+PI = 3.196818 | computed in 0.05 s
+
+Nodes: 2, Processes: 32
+PI = 3.169265 | computed in 0.09 s
+*/
 
 int main(int argc, char *argv[])
 {
